7586e38ba8838bfb6b7b6d9a15551c710f76411c
B Kleinen <kleinen@infrastructure.de>
2018-05-12 15:05:22 +0200

    initial commit

---
+++ prelab-and-primes/PreLab.java
@@ -0,0 +1,31 @@
+import java.util.*;
+/**
+ * Fill in your solutions for the PreLab in
+ * the corresponding methods.
+ * 
+ * @author Barne Kleinen
+ */
+public class PreLab
+{
+    public void printMultiplesOf5()
+    {
+
+    }
+
+    public int sumBetween(int a, int b){
+        return -1;
+    }
+
+    public void printStudentList(Collection<Student> imi1)
+    {
+    }
+    
+    public boolean isPrime(int n){
+        return false;
+    }
+    
+    public int countPrimesSmallerThan1000(){
+        return 0;
+    }
+    
+}
---
+++ prelab-and-primes/Student.java
@@ -0,0 +1,17 @@
+class Student{
+    
+    String firstName, surname;
+    
+    Student(String firstName, String surname){
+        this.firstName = firstName; 
+        this.surname = surname;
+    }
+
+    String getFirstName(){
+        return firstName;
+    }
+
+    String getSurname(){
+        return surname;
+    }
+}
